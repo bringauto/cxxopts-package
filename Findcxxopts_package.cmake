44f8f2cdfd2eeca9244223214bd9b38cf41b58c8
Jan Kubalek <janmkubalek@gmail.com>
2021-07-11 13:59:02 +0200

    First working verions

--- Findcxxopts_package.cmake
+++ Findcxxopts_package.cmake
@@ -3,13 +3,15 @@
 # Download and initialize variable cxxopts_DIR
 #
 
-CMLIB_DEPENDNECY(
+CMLIB_DEPENDENCY(
 	URI "https://github.com/bringauto/cxxopts-package/releases/download/v2.2.1/libcxxopts-dev_v2.2.1_any.zip"
 	TYPE ARCHIVE
-	OUTPUT_PATH_VAR _cxxopts_DIR
+	OUTPUT_PATH_VAR _cxxopts_ROOT
 )
 
-SET(cxxopts_DIR "${_cxxopts_DIR}"
+SET(cxxopts_ROOT "${_cxxopts_ROOT}"
 	CACHE STRING
-	"cxxopts dir"
+	"cxxopts root directory"
+	FORCE
 )
+
